65f00adeec90995f35ef71052e1b3e7ac212a5e3
Simone <cirino.simone99@gmail.com>
2022-11-24 14:34:47 +0100

    Aggiunta possibilità di vedere tutti gli ingredienti

---
+++ Controllers/IngredientController.cs
@@ -0,0 +1,21 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class IngredientController : Controller
+    {
+        PizzeriaDbContext db;
+        public IngredientController() : base()
+        {
+            db = new PizzeriaDbContext();
+        }
+        public IActionResult Index()
+        {
+            List<Ingredient> ingredients = db.Ingredients.Include("Pizze").ToList();
+            return View(ingredients);
+        }
+    }
+}
